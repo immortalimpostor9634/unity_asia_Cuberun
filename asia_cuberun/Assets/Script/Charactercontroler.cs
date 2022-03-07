d1d6d8959f7ad56ddb83f02c9277fc0a7b36d118
immortalimpostor9634 <90890242+immortalimpostor9634@users.noreply.github.com>
2022-03-07 15:33:03 +0800

    檢測是否接觸地面

--- asia_cuberun/Assets/Script/Charactercontroler.cs
+++ asia_cuberun/Assets/Script/Charactercontroler.cs
@@ -6,9 +6,18 @@ public class Charactercontroler : MonoBehaviour
 {
     private Rigidbody playerRB;
 
+    public float jumpForce;     //跳躍力道
+
+    public float gravityModifier;     //系統物理重力
+
+    public bool isOnGround = true;     //檢測是否在地面
+
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+
+        Physics.gravity *= gravityModifier;   
+        //等同Physics.gravity * Physics.gravity = gravityModifier
 
     }
 
@@ -17,6 +26,15 @@ public class Charactercontroler : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             playerRB.AddForce(Vector3.up * 10,ForceMode.Impulse);
+
+            isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+
+        isOnGround = true;
+
     }
 }
